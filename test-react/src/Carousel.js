7de8a81858407478d645c39365241cd09ad1ec6a
Rishabh <rishabh@mobiux.in>
2024-11-27 14:09:15 +0530

    chore: added manual test setup

--- test-react/src/Carousel.js
+++ test-react/src/Carousel.js
@@ -1,13 +1,3 @@
-import React from 'react';
-import {createComponent} from '@lit/react';
-import MobiCarousel from '@mobimedia/carousel';
-
-const Carousel = createComponent({
-  tagName: 'mobi-carousel',
-  elementClass: MobiCarousel,
-  react: React,
-  events: {},
-});
-// import Carousel from '@mobimedia/carousel-react';
+import Carousel from '@mobimedia/carousel-react';
 
 export default Carousel;
